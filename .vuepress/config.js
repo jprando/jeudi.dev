eefe71aff78be6aa34dfa08b1be4c138336e10b2
Jeudi Prando <jeudiprando@gmail.com>
2019-03-02 04:06:04 -0300

    Testando atributos head meta

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,22 @@
 module.exports = {
-  title: 'Jeudi Prando',
-  description: "jeudi's blog - criado em março de 2019"
+  title: ' Jeudi Prando ',
+  description: 'compartilhando algumas coisas que aprendi',
+  serviceWorker: true,
+  head: [
+    ['meta', { name: 'author', content: 'Jeudi Prando' }],
+    ['meta', { name: 'keywords', content: 'desenvolvimento' }]
+  ],
+  themeConfig: {
+    nav: [{ text: 'Início', link: '/' }],
+    lastUpdated: true, // 'Last Updated'
+    repo: 'jprando/jeudi.dev',
+    editLinks: true,
+    editLinkText: 'Ajude-nos a melhorar esta página!',
+    serviceWorker: {
+      updatePopup: {
+        message: 'Novo conteúdo está disponível.',
+        buttonText: 'Atualizar'
+      }
+    }
+  }
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+---
+home: true
+---
+
+_criado em março de 2019_
+
 Você esta no [Blog de Jeudi Prando](https://jeudi.dev)
