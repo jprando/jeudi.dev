bb90aa2de02381d12b668744033b3d38c46a8aa5
Jeudi Prando <jeudiprando@gmail.com>
2019-03-02 05:18:24 -0300

    imagem na tela inicial

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,13 +1,19 @@
 module.exports = {
   title: ' Jeudi Prando ',
-  description: 'compartilhando algumas coisas que aprendi',
+  description: 'algumas coisas que aprendi',
   serviceWorker: true,
   head: [
+    ['meta', { httpEquiv: 'Content-Language', content: 'pt-br' }],
+    ['meta', { name: 'language', content: 'Portuguese' }],
+    ['meta', { name: 'generator', content: 'vuepress' }],
+    ['meta', { name: 'no-email-collection', content: 'unspam.com' }],
+    ['meta', { name: 'rating', content: 'general' }],
     ['meta', { name: 'author', content: 'Jeudi Prando' }],
-    ['meta', { name: 'keywords', content: 'desenvolvimento' }]
+    ['meta', { name: 'contact', content: 'jeudiprando@gmail.com' }]
   ],
+  markdown: { lineNumbers: true },
   themeConfig: {
-    nav: [{ text: 'Início', link: '/' }],
+    // nav: [{ text: 'Início', link: '/' }],
     lastUpdated: true, // 'Last Updated'
     repo: 'jprando/jeudi.dev',
     editLinks: true,
--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 ---
+meta:
+  - name: keywords
+    content: inicio, blog, dicas, knowbase
 home: true
+heroImage: /hero.png
+actionText: Entrar →
+actionLink: /guide/
+features:
+  - title: Motivação
+    details: Criar e manter uma base de conhecimento para consultar em qualquer lugar e a qualquer hora.
+  - title: A origem
+    details: Problema resolvidos, Estudos, testes e pesquisas realizadas por mim é a origem dos assuntos que irei compartilhar aqui.
+footer: criado em março de 2019
 ---
-
-_criado em março de 2019_
-
-Você esta no [Blog de Jeudi Prando](https://jeudi.dev)
